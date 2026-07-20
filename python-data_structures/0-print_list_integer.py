d6b8955144b2b0e5767f48e898aeff9e3354eb5f
amudaheranwa <a.mudaheran@alustudent.com>
2026-07-20 06:02:09 -0700

    Add print_list_integer function

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a function that prints all integers of a list."""
+
+
+def print_list_integer(my_list=[]):
+    """Print all integers of a list, one per line.
+
+    Args:
+        my_list (list): A list of integers.
+    """
+    for number in my_list:
+        print("{}".format(number))
